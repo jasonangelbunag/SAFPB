4e2ad9b9313ead637ae2418e92b7c24e2c0cec70
jasonangel <60204355+jasonangelbunag@users.noreply.github.com>
2024-01-11 09:32:21 +0800

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1j4k7Vr8g1S_3RJmoTjfmxzXPF-qPhBvf1EBWgPe7Scs/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyDmtxnXYsYXhCoo50pjw1V04yCAC_Wxj8A';
